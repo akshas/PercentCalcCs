d7719c15d6482c5b1c4e93dbd341d5214c70d572
Alex <allexxx81@gmail.com>
2020-10-13 16:00:56 +0200

    StartProj

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PercentCalcMvvm.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedPage = new PercentageViewModel();
+
+        public BaseViewModel SelectedPage
+        {
+            get { return _selectedPage ; }
+            set { _selectedPage  = value; }
+        }
+        public MainViewModel()
+        {
+        }
+
+    }
+}
