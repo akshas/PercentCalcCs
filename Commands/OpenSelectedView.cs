43022f000fdd5dcfd8b93310e183c8b2936d5128
Александр Кочетов <allexxx81@gmail.com>
2020-10-14 23:36:45 +0200

    Design ist fertig

---
+++ Commands/OpenSelectedView.cs
@@ -0,0 +1,43 @@
+﻿using PercentCalcMvvm.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PercentCalcMvvm.Commands
+{
+    class OpenSelectedView : ICommand
+    {
+
+        private MainViewModel viewModel;
+        public event EventHandler CanExecuteChanged;
+
+        public OpenSelectedView(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if ((string)parameter == "Percent")
+            {
+                viewModel.SelectedPage = new PercentViewModel(); 
+            }
+            else if ((string)parameter == "Percentage")
+            {
+                viewModel.SelectedPage = new PercentageViewModel(); 
+            }
+            else if ((string)parameter == "Sum")
+            {
+                viewModel.SelectedPage = new SumViewModel(); 
+            }
+        }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using PercentCalcMvvm.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PercentCalcMvvm.ViewModels
 {
@@ -13,10 +15,20 @@ namespace PercentCalcMvvm.ViewModels
         public BaseViewModel SelectedPage
         {
             get { return _selectedPage ; }
-            set { _selectedPage  = value; }
+            set
+            { 
+                _selectedPage  = value;
+                OnPropertyChanged(nameof(SelectedPage));
+            }
         }
         public MainViewModel()
         {
+            OpenSelectedView = new OpenSelectedView(this);
+        }
+
+        public ICommand OpenSelectedView
+        {
+            get; set;
         }
 
     }
