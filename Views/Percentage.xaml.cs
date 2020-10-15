b7778d7c5fe68662d0573753da92c49a641c1d7c
Александр Кочетов <allexxx81@gmail.com>
2020-10-15 23:39:22 +0200

    Disighn ist fast fertig

--- Views/Percentage.xaml.cs
+++ Views/Percentage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PercentCalcMvvm.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace PercentCalcMvvm.Views
         public Percentage()
         {
             InitializeComponent();
+            DataContext = new PercentageViewModel();
         }
     }
 }
